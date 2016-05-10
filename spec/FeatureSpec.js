498c74eec2babcbd05a02436340836d3ae4648e8
chriselevate <coates.c@hotmail.com>
2016-05-10 15:49:28 +0100

    user story 3 complete

--- spec/FeatureSpec.js
+++ spec/FeatureSpec.js
@@ -13,11 +13,17 @@ describe('Feature Test:', function(){
     plane.land(airport);
     expect(airport.planes()).toContain(plane);
   });
-  
+
   it('plane is no longer in the airport when it take off',function(){
-    plane.land(airport);  
+    plane.land(airport);
     plane.takeoff();
     expect(airport.planes()).not.toContain(plane);
   });
-});
 
+  it('blocks takeoff when weather is stormy', function(){
+    plane.land(airport)
+    spyOn(airport,'isStormy').and.returnValue(true);
+    expect(function(){ plane.takeoff();}).toThrowError('cannot takeoff during storm');
+    expect(airport.planes()).toContain(plane);
+  });
+});
